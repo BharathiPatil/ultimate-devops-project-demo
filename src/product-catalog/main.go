43f5e13ae00fed50d765e5683ecc9a09831d0be6
Bharathi413 <bharathipatilv@gmail.com>
2025-06-22 15:58:44 +0000

    chore : verigy github

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -252,7 +252,7 @@ func (p *productCatalog) GetProduct(ctx context.Context, req *pb.GetProductReque
 
 	// GetProduct will fail on a specific product when feature flag is enabled
 	if p.checkProductFailure(ctx, req.Id) {
-		msg := fmt.Sprintf("Error: Product Catalog Fail Feature Flag En")
+		msg := fmt.Sprintf("Error: Product Catalog Fail Feature Flag")
 		span.SetStatus(otelcodes.Error, msg)
 		span.AddEvent(msg)
 		return nil, status.Errorf(codes.Internal, msg)
